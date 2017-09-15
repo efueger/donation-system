4e6b0b8a7b86193ca9b294fdfb9a14de342452e1
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-09-15 15:05:57 +0100

    Plug the supporter data class in the database class

--- lib/salesforce_database.rb
+++ lib/salesforce_database.rb
@@ -2,6 +2,7 @@
 
 require 'restforce'
 require 'salesforce/donation_data'
+require 'salesforce/supporter_data'
 
 class SalesforceDatabase
   def initialize(client = Restforce.new)
@@ -14,20 +15,18 @@ class SalesforceDatabase
     [] if donation_id
   end
 
-  def ensure_supporter(data)
-    search_supporter_by_email(data.email) || create_supporter(data)
-  end
-
-  def search_supporter_by_email(email)
-    supporters = search(search_supporter_by_email_query(email))
-    select_first_entered_supporter(supporters)
+  def search_supporter_by(field, value)
+    supporters = search(search_supporter_by_query(field, value))
+    first_entered = select_first_entered_supporter(supporters)
+    return unless first_entered
+    find(Salesforce::SupporterData::TABLE_NAME, first_entered['Id'])
   end
 
   def create_supporter(data)
-    attributes = salesforce_supporter_required_fields(data)
-                 .merge(supporter_required_fields(data))
-    supporter_id = create('Contact', attributes)
-    search(search_supporter_by_id_query(supporter_id)).first
+    sobject_name = Salesforce::SupporterData::TABLE_NAME
+    sobject_fields = Salesforce::SupporterData.new(data).fields
+    supporter_id = create(sobject_name, sobject_fields)
+    find(Salesforce::SupporterData::TABLE_NAME, supporter_id)
   end
 
   RawDonationData = Struct.new(:amount, :account_id)
@@ -50,31 +49,23 @@ class SalesforceDatabase
     client.create!(sobject_name, sobject_fields)
   end
 
+  def find(sobject_name, id)
+    client.find(sobject_name, id)
+  end
+
+  def ensure_supporter(data)
+    search_supporter_by(:Email, data.email) || create_supporter(data)
+  end
+
   def select_first_entered_supporter(supporters)
     supporters.sort_by { |contact| contact['First_entered__c'] }.first
   end
 
-  def search_supporter_by_email_query(email)
+  def search_supporter_by_query(field, value)
     %(
-      select Email, First_entered__c, AccountId, LastName
-      from Contact
-      where Email='#{email}'
+      select Id, First_entered__c
+      from #{Salesforce::SupporterData::TABLE_NAME}
+      where #{field}='#{value}'
     ).gsub(/\s+/, ' ').strip
-  end
-
-  def search_supporter_by_id_query(id)
-    %(
-      select Email, First_entered__c, AccountId, LastName
-      from Contact
-      where Id='#{id}'
-    ).gsub(/\s+/, ' ').strip
-  end
-
-  def salesforce_supporter_required_fields(data)
-    { LastName: data.name }
-  end
-
-  def supporter_required_fields(data)
-    { Email: data.email }
   end
 end
--- spec/salesforce_database_spec.rb
+++ spec/salesforce_database_spec.rb
@@ -6,19 +6,17 @@ require 'spec_helper'
 SalesforceData = Struct.new(:amount, :email, :name)
 
 RSpec.describe SalesforceDatabase do
-  describe 'supporter exists', vcr: { record: :once } do
+  describe 'supporter exists', vcr: { record: :all } do
     let(:client) { Restforce.new(host: 'cs70.salesforce.com') }
     let(:database) { SalesforceDatabase.new(client) }
 
     it 'finds an existing supporter by email' do
-      data = SalesforceData.new('irrelevant', 'test@test.com')
-      found = database.search_supporter_by_email(data.email)
+      found = database.search_supporter_by(:Email, 'test@test.com')
       expect(found[:Email]).to eq('test@test.com')
     end
 
     it 'picks oldest supporter if several exist with same email' do
-      data = SalesforceData.new('irrelevant', 'repeated_email@test.com')
-      found = database.search_supporter_by_email(data.email)
+      found = database.search_supporter_by(:Email, 'repeated_email@test.com')
       expect(found[:First_entered__c]).to eq('2017-08-01')
     end
 
@@ -26,68 +24,22 @@ RSpec.describe SalesforceDatabase do
       data = SalesforceData.new('20', 'test@test.com')
       expect(database.add_donation(data)).to eq([])
     end
-
-    it 'attaches the donation to a supporter' do
-      client_mock = instance_double(Restforce::Concerns::API).as_null_object
-      allow(client_mock).to receive(:query).and_return(supporter_data)
-      database_with_mock = SalesforceDatabase.new(client_mock)
-      data = SalesforceData.new('20', 'test@test.com')
-
-      database_with_mock.add_donation(data)
-
-      expect(client_mock).to have_received(:create!).with(
-        'Opportunity',
-        Amount: '20',
-        CloseDate: '2017-09-11',
-        Name: 'Online donation',
-        StageName: 'Received',
-        AccountId: '0013D00000LBYutQAH'
-      )
-    end
-
-    def supporter_data
-      [
-        {
-          'attributes' => {
-            'type' => 'Contact',
-            'url' => '/services/data/v38.0/sobjects/Contact/0033D00000KoEluQAF'
-          },
-          'Email' => 'test@test.com', 'First_entered__c' => '2017-09-11',
-          'AccountId' => '0013D00000LBYutQAH'
-        }
-      ]
-    end
   end
 
-  describe 'supporter does not exist', vcr: { record: :once } do
+  describe 'supporter does not exist', vcr: { record: :all } do
     let(:client) { Restforce.new(host: 'cs70.salesforce.com') }
     let(:database) { SalesforceDatabase.new(client) }
+
+    it 'returns nothing if there is no supporter with that email' do
+      found = database.search_supporter_by(:Email, 'i-dont-exist@test.com')
+      expect(found).to be_nil
+    end
 
     it 'knows how to create a supporter with the right fields' do
       data = SalesforceData.new('irrelevant', 'a-supporter@test.com', 'A Name')
       supporter = database.create_supporter(data)
       expect(supporter[:LastName]).to eq('A Name')
-    end
-
-    it 'creates a supporter if it can not be found by email' do
-      data = SalesforceData.new('20', 'a-supporter@test.com', 'A Name')
-      expect(database.add_donation(data)).to eq([])
-    end
-
-    it 'creates the supporter with an email' do
-      client_mock = instance_double(Restforce::Concerns::API).as_null_object
-      allow(client_mock).to receive(:create!).and_return('1')
-      allow(client_mock).to receive(:query).and_return([])
-      database_with_mock = SalesforceDatabase.new(client_mock)
-      data = SalesforceData.new('irrelevant', 'a-supporter@test.com', 'A Name')
-
-      database_with_mock.create_supporter(data)
-
-      expect(client_mock).to have_received(:create!).with(
-        'Contact',
-        LastName: 'A Name',
-        Email: 'a-supporter@test.com'
-      )
+      expect(supporter[:Email]).to eq('a-supporter@test.com')
     end
   end
 end
